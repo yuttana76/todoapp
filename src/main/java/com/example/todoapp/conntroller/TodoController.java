0844b36877b52d5f7637171e28c0235da6ddae26
yuttana76 <yuttana76@gmail.comm>
2018-03-02 15:07:39 +0700

    Fish todos API & Create Front-end(Angular)

--- src/main/java/com/example/todoapp/conntroller/TodoController.java
+++ src/main/java/com/example/todoapp/conntroller/TodoController.java
@@ -1,12 +1,12 @@
 package com.example.todoapp.conntroller;
 
-import com.example.todoapp.models.Todo;
-
 import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.todoapp.models.Todo;
 import com.example.todoapp.repositories.TodoRepository;
 
 @RestController
@@ -28,48 +29,64 @@ import com.example.todoapp.repositories.TodoRepository;
 @CrossOrigin("*")
 public class TodoController {
 
+	private static final Logger log = LoggerFactory.getLogger(TodoController.class);
+	
 	@Autowired
     TodoRepository todoRepository;
 	
 	@GetMapping("/todos")
     public List<Todo> getAllTodos() {
+		log.info("Welcome GET todos");
         Sort sortByCreatedAtDesc = new Sort(Sort.Direction.DESC, "createdAt");
         return todoRepository.findAll(sortByCreatedAtDesc);
     }
 	
 	@PostMapping("/todos")
     public Todo createTodo(@Valid @RequestBody Todo todo) {
+		log.info("Welcome POST todos");
         todo.setCompleted(false);
         return todoRepository.save(todo);
     }
 	
-//	@GetMapping(value="/todos/{id}")
-//    public ResponseEntity<Todo> getTodoById(@PathVariable("id") String id) {
-//        Todo todo = todoRepository.findOne(id);
-//        if(todo == null) {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        } else {
-//            return new ResponseEntity<>(todo, HttpStatus.OK);
-//        }
-//    }
-//	
-//	@PutMapping(value="/todos/{id}")
-//    public ResponseEntity<Todo> updateTodo(@PathVariable("id") String id,
-//                                           @Valid @RequestBody Todo todo) {
-//        Todo todoData = todoRepository.findOne(id);
-//        if(todoData == null) {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        }
-//        todoData.setTitle(todo.getTitle());
-//        todoData.setCompleted(todo.getCompleted());
-//        Todo updatedTodo = todoRepository.save(todoData);
-//        return new ResponseEntity<>(updatedTodo, HttpStatus.OK);
-//    }
+	@GetMapping(value="/todos/{id}")
+    public ResponseEntity<Optional<Todo>> getTodoById(@PathVariable("id") String id) {
+		
+		log.info("Welcome POST todos("+id+")");
+		
+		Optional<Todo> todo = todoRepository.findById(id);
+        
+        if(todo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(todo, HttpStatus.OK);
+        }
+    }
 	
-	 
+	@PutMapping(value="/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable("id") String id,
+                                           @Valid @RequestBody Todo todo) {
+		
+		log.info("Welcome PUT todos("+id+")");
+		
+		Optional<Todo> todoData = todoRepository.findById(id);
+        
+        if(!todoData.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        
+        todoData.get().setTitle(todo.getTitle());
+        todoData.get().setCompleted(todo.getCompleted());
+        Todo updatedTodo = todoRepository.save(todoData.get());
+        return new ResponseEntity<>(updatedTodo, HttpStatus.OK);
+    }
+	
 	@DeleteMapping(value="/todos/{id}")
     public void deleteTodo(@PathVariable("id") String id) {
+		
+		log.info("Welcome DELETE todos("+id+")");
+		
         todoRepository.deleteById(id);
     }
+	
 	
 }
--- src/main/java/com/example/todoapp/repositories/TodoRepository.java
+++ src/main/java/com/example/todoapp/repositories/TodoRepository.java
@@ -1,11 +1,11 @@
 package com.example.todoapp.repositories;
 
+
+import com.example.todoapp.models.Todo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.todoapp.models.Todo;
-
 @Repository
 public interface TodoRepository extends MongoRepository<Todo, String> {
-
+	
 }
